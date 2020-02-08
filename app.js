3c1ac28932c606e4dcbb324f4ecdc5255e9a7590
sergiysiomko <sergiysiomko@outlook.com>
2020-02-09 01:57:17 +0200

    delete sertificate

--- app.js
+++ app.js
@@ -38,8 +38,6 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 
-app.use(secure)
-
 //app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
