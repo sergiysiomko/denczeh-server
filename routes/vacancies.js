69f3464a25231880d8fb5ec7777ecdcd06ddf8b6
s.siomko <s.siomko@viseven.com>
2021-08-06 03:18:07 +0300

    fix

--- routes/vacancies.js
+++ routes/vacancies.js
@@ -29,6 +29,24 @@ router.get('/list', passport.isLoggedIn, controller.getList);
 
 router.get('/crm-api-refresh-token', passport.isLoggedIn, controller.refreshToken);
 
+router.use((req, res, next) => {
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'https://denysiukjob.com.ua');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
 router.post('/lead', controller.addLead);
 
 router.get('/:link', controller.getVacancy);
